ee1dbdc0f02ae78022d8d61fdd0c54d44326ddfb
Mohamed Donzo <demahom18@outlook.com>
2021-02-05 19:44:31 -0500

    fix: added router link for button back

--- src/views/CountryDetail.vue
+++ src/views/CountryDetail.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="btn-wrap">
-    <div class="btn-back">
+    <router-link :to="{ name: 'Home' }">
+      <div class="btn-back">
       <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M5.81802 3.6967L6.87868 4.75736L3.3785 8.25754H16.7428L16.7428 9.74246H3.3785L6.87868 13.2426L5.81802 14.3033L0.514719 9L5.81802 3.6967Z" fill="#111517"/>
       </svg>
       <span>Back</span>
     </div>
+    </router-link>
   </div>
   <div class="detail">
     <div class="flag">
