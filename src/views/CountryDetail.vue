6ecdeae5cde52390f08c8871c0b26f6286a0ffe8
Mohamed Donzo <md@mambomambo.com>
2022-02-10 02:40:00 -0500

    fix code formatting

--- src/views/CountryDetail.vue
+++ src/views/CountryDetail.vue
@@ -1,237 +1,155 @@
 <template>
-  <div class="country-detail-page" v-if="country">
-    <div class="btn-wrap">
-      <router-link :to="{ name: 'Home' }">
-        <div class="btn back">
-          <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M5.81802 3.6967L6.87868 4.75736L3.3785 8.25754H16.7428L16.7428 9.74246H3.3785L6.87868 13.2426L5.81802 14.3033L0.514719 9L5.81802 3.6967Z" fill="#111517"/>
-          </svg>
-          <span>Back</span>
+    <div class="country-detail-page" v-if="country">
+        <div class="btn-wrap">
+        <router-link :to="{ name: 'Home' }">
+            <div class="btn back">
+            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M5.81802 3.6967L6.87868 4.75736L3.3785 8.25754H16.7428L16.7428 9.74246H3.3785L6.87868 13.2426L5.81802 14.3033L0.514719 9L5.81802 3.6967Z" fill="#111517"/>
+            </svg>
+            <span>Back</span>
+            </div>
+        </router-link>
         </div>
-      </router-link>
-    </div>
-    <div class="detail">
-      <div class="flag">
-        <img 
-          :src="country.flags.png" 
-          :alt="`${country.name.common} flag`"
-        />
-      </div>
-      <div class="infos">
-        <h2>{{ country.name.common }}</h2>
-        <div class="infos1">
-          <p>
-            <b>Native name: </b>{{ countryNativeName }}<br> 
-            <b>Population: </b>{{ population }}<br>   
-            <b>Region: </b>{{ country.region }}<br> 
-            <b>Sub Region: </b>{{ country.subregion }}<br>  
-            <b>Capital: </b>{{ country.capital }}<br>
-          </p>
-          <p>
-            <b>Top Level Domain: </b>
-            <span
-              v-for="(domain, i) in country.tld"
-              :key="domain"
-            >{{ domain }} {{ i > 0 && i != country.tld.length ? ',' : '' }}
-            </span> <br>
+        <div class="detail">
+            <div class="flag">
+                <img 
+                    :src="country.flags.png" 
+                    :alt="`${country.name.common} flag`"
+                />
+            </div>
+            <div class="infos">
+                <h2>{{ country.name.common }}</h2>
+                <div class="infos1">
+                    <p>
+                        <b>Native name: </b>{{ countryNativeName }}<br> 
+                        <b>Population: </b>{{ population }}<br>   
+                        <b>Region: </b>{{ country.region }}<br> 
+                        <b>Sub Region: </b>{{ country.subregion }}<br>  
+                        <b>Capital: </b>{{ country.capital }}<br>
+                    </p>
+                    <p>
+                        <b>Top Level Domain: </b>
+                        <span
+                        v-for="(domain, i) in country.tld"
+                        :key="domain"
+                        >{{ domain }} {{ i > 0 && i != country.tld.length ? ',' : '' }}
+                        </span> <br>
 
-            <b>Currencies: </b>
-            <span 
-              v-for="currency in country.currencies"
-              :key="currency.code"
-            >{{ currency.name }}
-            </span><br> 
+                        <b>Currencies: </b>
+                        <span 
+                        v-for="currency in country.currencies"
+                        :key="currency.code"
+                        >{{ currency.name }}
+                        </span><br> 
 
-            <b>Languages: </b>
-            <span 
-              v-for="(lang, i) in countryLanguages"
-              :key="`lang` + i"
-              class="language"
-            >{{ lang + ', '}}
-            </span><br> 
-          </p>
+                        <b>Languages: </b>
+                        <span 
+                        v-for="(lang, i) in countryLanguages"
+                        :key="`lang` + i"
+                        class="language"
+                        >{{ lang + ', '}}
+                        </span><br> 
+                    </p>
+                </div>
+                <div class="borders">
+                    <p>
+                        <b>Border Countries: </b>
+                        <div>
+                            <span 
+                                v-for="border in borders" 
+                                :key="border + Math.random()*100"
+                                class="border-pill btn"
+                                @click="gotoPage(border)"
+                            > {{ border }}
+                            </span>
+                        </div> 
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="borders">
-          <p>
-            <b>Border Countries: </b>
-            <div>
-              <span 
-                v-for="border in borders" 
-                :key="border + Math.random()*100"
-                class="border-pill btn"
-                @click="gotoPage(border)"
-              > {{ border }}
-              </span>
-            </div> 
-          </p>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 <script>
-import { computed, inject, ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { inject } from 'vue'
 
 export default {
-  props: {
-    countryname: {
-      type: String,
-      required: true
-    }
-  },
-  setup(props) {
-    // const router = useRouter()
-    const countries = inject('countries')
-
-    // //
-    // const country = computed(() => {
-    //   return countries.value
-    //     .find(country => country.name.common === props.countryname)
-    // })
-
-    // //
-    // const countryLanguages = computed(() => {
-    //     let langs = []
-    //     for (let lang in country.value.languages) {
-    //         langs.push(country.value.languages[lang])
-    //         console.log(lang);
-    //     }
-
-    //     return langs
-    // })
-
-    // //
-    // const countryNativeName = computed(() => {
-    //     let names = []
-    //     for (let name in country.value.name.nativeName) {
-    //         return country.value.name.nativeName[name]
-    //     }
-
-    //     return names
-    // })
-
-    // //
-    // const populationFormatted = computed(()=> {
-    //     //There's probably a more efficient way to do it
-    //     return [...country.value.population.toString()]
-    //         .reverse().map((num, idx) => (idx % 3 === 0 && idx != 0) ? `${num},` : num)
-    //         .reverse().join('')
-    // }) 
-
-    // //
-    // const borders = computed(() => {
-    //   let namesRaw = ref([]) 
-    //   let bordersRaw = country.value.borders ? Array.from(country.value.borders) : []
-
-    // if (bordersRaw.length = 0) {
-    //     countryBorders = null
-    //     return
-    // }
-        
-    //   // No direct access to the borders' name so we
-    //   // Get the names of the borders with the fifa (I don't know what this is) 
-    //   for (let i=0, l = bordersRaw.length; i < l; i++) {
-    //     namesRaw.value.push(countries.value
-    //       .filter(country => country.fifa == bordersRaw[i]))
-    //   }
-
-    //   // Get the names from the raw values
-    //   countryBorders.value = namesRaw.value.map( nRaw =>  nRaw[0].name.common)
-        
-    //   // Then format the names to get rid of the parenthesis
-    //   let namesFormatted = ref([])
-    //   countryBorders.value.forEach(bord => {
-    //       namesFormatted.value.push(bord.toString().split(' ('))
-    //     })
-        
-    //   // Take the element at index 0 to shorten the name
-    //   return namesFormatted.value.map(name => name[0])
-    // })
-    
-    // const gotoPage = border => {
-    //   let countryToGo = this.borders
-    //     .find(c => c.includes(border))
-
-    //   router.push({
-    //     name: 'CountryDetail', 
-    //     params: { countryname: countryToGo},
-        
-    //   })
-    // }
-         
-    return { 
-      countries, 
-    //   gotoPage,
-    }
-  },
-  methods: {
-      gotoPage(border) {
-        let countryToGo = this.borders
-            .find(c => c.includes(border))
-
-        this.$router.push({
-            name: 'CountryDetail', 
-            params: { countryname: countryToGo},
-            
-        })
-      },
-      getBorderName(code) {
-          let border = this.countries.filter(country => {
-              return country.fifa == code || country.cca3 == code || country.cioc == code
-          })[0]
-        console.log(border);
-
-          return border.name.common
-      }
-  },
-  computed: {
-      country() {
-        if (this.countries && this.countries.length)
-            return this.countries
-                .find(country => country.name.common === this.countryname)  
-      },
-      countryLanguages() {
-        let langs = []
-        for (let lang in this.country.languages) {
-            langs.push(this.country.languages[lang])
+    props: {
+        countryname: {
+        type: String,
+        required: true
         }
+    },
+    setup() {
+        const countries = inject('countries')
 
-        return langs
-      },
-      countryNativeName() {
-        let names = []
-        for (let name in this.country.name.nativeName) {
-            // console.log('Hello');
-            return this.country.name.nativeName[name].official
+        return { 
+        countries,
         }
+    },
+    methods: {
+        gotoPage(border) {
+            let countryToGo = this.borders
+                .find(c => c.includes(border))
 
-        return names
-      },
-      population() {
-          return [...this.country.population.toString()]
-            .reverse().map((num, idx) => (idx % 3 === 0 && idx != 0) ? `${num},` : num)
-            .reverse().join('')
-      },
-      borders() {
-        let bordersRaw = this.country.borders ? Array.from(this.country.borders) : []
-        let _borders = []
+            this.$router.push({
+                name: 'CountryDetail', 
+                params: { countryname: countryToGo},
+                
+            })
+        },
+        getBorderName(code) {
+            let border = this.countries.filter(country => {
+                return country.fifa == code || country.cca3 == code || country.cioc == code
+            })[0]
+            console.log(border);
 
-        if (bordersRaw.length == 0) {
+            return border.name.common
+        }
+    },
+    computed: {
+        country() {
+            if (this.countries && this.countries.length)
+                return this.countries
+                    .find(country => country.name.common === this.countryname)  
+        },
+        countryLanguages() {
+            let langs = []
+            for (let lang in this.country.languages) {
+                langs.push(this.country.languages[lang])
+            }
+
+            return langs
+        },
+        countryNativeName() {
+            let names = []
+            for (let name in this.country.name.nativeName) {
+                return this.country.name.nativeName[name].official
+            }
+
+            return names
+        },
+        population() {
+            return [...this.country.population.toString()]
+                .reverse().map((num, idx) => (idx % 3 === 0 && idx != 0) ? `${num},` : num)
+                .reverse().join('')
+        },
+        borders() {
+            let bordersRaw = this.country.borders ? Array.from(this.country.borders) : []
+            let _borders = []
+
+            if (bordersRaw.length == 0) {
+                return _borders
+            }
+
+            for (let countryCode of bordersRaw) {
+                let border = this.getBorderName(countryCode)
+                _borders.push(border)
+            }
+
             return _borders
         }
-
-        for (let countryCode of bordersRaw) {
-                 
-            let border = this.getBorderName(countryCode)
-            _borders.push(border)
-        }
-
-        return _borders
-      }
-
-  },
-  
+    },
+    
 }
 </script>
 <style lang="scss" >
